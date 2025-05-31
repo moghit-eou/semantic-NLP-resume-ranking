f16a2c660267efbbcd68c7027a02a9a4cf47585a
moghit_ad <moghit.elouaad1@gmail.com>
2025-05-31 23:50:17 +0100

    resume to db

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, jsonify , render_template
 import json 
+from pymongo import MongoClient
 
-
+client = MongoClient("mongodb://localhost:27017/")
+db = client["pfe_db"]
+collection = db["students"]
 
 app = Flask(__name__)
 @app.route('/')
@@ -18,8 +21,17 @@ def add_resume():
     if not data:
         data = request.args.get("argument")  # fallback to query parameter if form data is not present
 
-    json_resume = resume_text_to_json(data)
+    json_resume = resume_text_to_json(data) 
+
+    result = collection.insert_one(json_resume)    
+    json_resume["_id"] = str(result.inserted_id)
+
     return json_resume
+
+
+
+
+
 
 
 
@@ -29,5 +41,6 @@ def resume_text_to_json(resume):
     data = json.loads(clean_text)
     return data 
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True , host="0.0.0.0", port=5000)
