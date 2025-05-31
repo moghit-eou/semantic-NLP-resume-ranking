0f6bcd1acd7ba143c48a207cfc3a549015522384
moghit_ad <moghit.elouaad1@gmail.com>
2025-05-31 02:09:18 +0100

    json

--- app.py
+++ app.py
@@ -1,22 +1,33 @@
 from flask import Flask, request, jsonify , render_template
-app = Flask(__name__)
+import json 
 
+
+
+app = Flask(__name__)
 @app.route('/')
 def hello():
     return render_template('index.html')
 
 
 
-@app.route('/adding' , methods=['POST' , 'GET'])
-def check():
+@app.route('/add_resume' , methods=['POST' , 'GET'])
+def add_resume():
 
     data = request.form.get('argument') # this was taken from the HTML form
 
     if not data:
         data = request.args.get("argument")  # fallback to query parameter if form data is not present
 
-    return jsonify({'message': f'You entered: {data}'})
+    json_resume = resume_text_to_json(data)
+    return json_resume
 
+
+
+
+def resume_text_to_json(resume):
+    clean_text = resume.encode().decode('unicode_escape')
+    data = json.loads(clean_text)
+    return data 
 
 if __name__ == '__main__':
     app.run(debug=True)
